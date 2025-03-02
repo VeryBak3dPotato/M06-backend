163a7cdf4788cc5b80143c0e951a68d6796afc0d
Connor Bailey <cebailey92@gmail.com>
2025-03-02 14:03:40 -0500

    added delete and new user

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 // Setup
 const express = require('express');
+// const bodyParser = require("body-parser");
+const jwt = require("jwt-simple");
+const User = require("./models/users");
+const Song = require("./models/songs");
 var cors = require('cors');
 
 // Activates app as an express application
-const bodyParser = require("body-parser");
-const Song = require("./models/songs");
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 const router = express.Router();
+const secret = "supersecret";
+
+// Create a new user
+router.post("/user", async (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        res.status(400).json({error: "Missing username or password"});
+    }
+    const newUser = await new User({
+        username: req.body.username,
+        password: req.body.password,
+        status: req.body.status
+    });
+    try {
+        await newUser.save();
+        res.sendStatus(201);
+    } catch {
+        res.status(400).json({error: "Username already exists"});
+    }
+});
 
 // GET all songs in a database
 router.get("/songs", async (req, res) => {
@@ -50,6 +71,17 @@ router.put("/songs/:id", async (req, res) => {
         const song = req.body;
         await Song.updateOne({_id: req.params.id}, song);
         res.json(song);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+// DELETE a song by ID
+router.delete("/songs/:id", async (req, res) => {
+    try {
+        const song = await Song.findById(req.params.id);
+        await Song.deleteOne({_id: song._id});
+        res.sendStatus(204);
     } catch (err) {
         res.status(400).send(err);
     }
